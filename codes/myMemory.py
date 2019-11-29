44559d1cfa02423246d676589cdf51b3898cb237
Mudasir <ms03831@st.habib.edu.pk>
2019-11-29 12:47:24 +0500

    memory class added

---
+++ codes/myMemory.py
@@ -0,0 +1,14 @@
+class Memory:
+	memoryFootprint = 0
+
+	def __init__(self):
+		pass
+
+	def updateMemory(int n);
+		""" 
+		increases memory footprint by n
+		"""
+		memoryFootprint += n
+
+	def resetMemory():
+		memoryFootprint = 0

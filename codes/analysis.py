c943ee44537366d4c2abc20026e09e7f6b78d325
Mudasir <ms03831@st.habib.edu.pk>
2019-11-29 13:01:52 +0500

    analysis file added incomplete lol

--- codes/analysis.py
+++ codes/analysis.py
@@ -1,3 +1,21 @@
-from MyTime import *
+from myTime import *
+from myMemory import *
+
+
+class TestCases:
+	def __init__(self):
+		self.n = [10, 25, 50, 100, 500, 1000, 5000, 10000]
+		self.testsConstantCapacity = dict()
+		self.testsVaryingCapacity = dict()
+
+	def __generateTests(self):
+		for numberItems in self.n:
+			"""
+			self.testsVaryingCapacity[numberItems] = [[(i, random.randint(1, 2 + int(numberItems/10)), random.randint(1, 2+int(numberItems/5))) 
+						for i in range(numberItems)], random.randint(max(int(numberItems/10), 20), max(int(numberItems/5), 100))]
+			self.testsConstantCapacity[numberItems] = [[(i, random.randint(1, 2 + int(numberItems/10)), random.randint(1, 2+int(numberItems/5))) 
+						for i in range(numberItems)], 1]
+			"""
 
 class Analyze:
+	def __init__(self):

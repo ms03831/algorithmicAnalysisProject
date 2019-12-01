0924d912eb4a3d37818d19494d0c1217b3d6ecb3
hf04097@st.habib.edu.pk <Serina_7904>
2019-12-01 18:39:51 +0500

    added memory update

--- codes/greedyAlgo.py
+++ codes/greedyAlgo.py
@@ -1,3 +1,5 @@
+from myMemory import *
+
 def greedyAlgo(weightValue,capacity):
     """
     :param weightValue: a list of tuples(item,weight,value)
@@ -5,19 +7,24 @@ def greedyAlgo(weightValue,capacity):
     :return: knapsack,a list of items in the sack
     """
     ratios_weight_value = []
+    Memory.updateMemory(1)
     ratios_index = list(range(len(weightValue)))
+    Memory.updateMemory(len(weightValue))
     knapsack =[]
+    Memory.updateMemory(1)
+
     for i in range(len(weightValue)):
         ratios_weight_value.append(weightValue[i][2]/weightValue[i][1])  #taking out value to weight ratio
+        Memory.updateMemory(1)
     ratios_index.sort(key=lambda i: ratios_weight_value[i], reverse=True)  #keeping track of the ratio indexes after sorting
+    Memory.updateMemory(len(weightValue)) # for sort
 
     for i in ratios_index:    #going to the index with max ratio first
+        Memory.updateMemory(1)
         if weightValue[i][1] <= capacity:
             capacity -= weightValue[i][1]
             knapsack.append(weightValue[i][0])
     return knapsack
 
 
-
-#print(greedyAlgo([("a",10,60),("b",40,40),("c",20,100),("d",30,120)],50))
 

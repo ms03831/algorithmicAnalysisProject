1ca2385d65df25fc9ef13bf895b49ce1319cd2bc
Kainat Abbasi <33037474+04051@users.noreply.github.com>
2019-12-01 17:35:52 +0500

    Update bruteForceKnapsack.py

--- codes/bruteForceKnapsack.py
+++ codes/bruteForceKnapsack.py
@@ -1,26 +1,46 @@
+import sys
+from myMemory import *
 
 def powerset(items):
     result = [[]]
     
+    Memory.updateMemory(len(items))
     for item in items:
         newset = [r+[item]for r in result]
         result.extend(newset)
+        
+        Memory.updateMemory(len(result))
+        Memory.updateMemory(len(newset))
+        
     return result
 
 def knapsack_brute_force(items, capacity):
     knapsack = []
     bestValue = 0
+    
+    Memory.updateMemory(len(items))
     output = ['0' for i in range(len(items))]
     
-    for element in powerset(items):
+   
+    powSet = powerset(items)
+    
+    Memory.updateMemory(len(powSet))
+    
+    for element in powSet:
+        Memory.updateMemory(len(element)*2)
+        
         setWeight = sum([e[1] for e in element])
         setValue = sum([e[2] for e in element])
+        
         if setValue > bestValue and setWeight <= capacity:
-    
             knapsack = element
-            
+    
+    Memory.updateMemory(len(items))
     for i in range(0, len(items)):
+        
+        Memory.updateMemory(len(knapsack))
         if items[i] in knapsack:
+            Memory.updateMemory(1)
             output[i] = 1
     #print('l', output)
             

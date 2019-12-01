63c210dd97209a32869138262fcd1667e8fb027b
Mudasir <ms03831@st.habib.edu.pk>
2019-12-02 00:10:10 +0500

    great analysis added

--- codes/analysis.py
+++ codes/analysis.py
@@ -9,6 +9,7 @@ from geneticKnapsack import *
 import matplotlib.pyplot as plt
 import seaborn as sns
 import random
+import numpy as np
 
 sns.set_style("darkgrid", {"axes.facecolor": ".9", "lines.linewidth": 2.5})
 
@@ -16,7 +17,7 @@ sns.set_style("darkgrid", {"axes.facecolor": ".9", "lines.linewidth": 2.5})
 
 class TestCases:
     def __init__(self):
-        self.n = [10, 25, 50, 100, 500, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000]
+        self.n = [10, 25, 50, 100, 500, 1000]#, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000]
         self.testsVaryingCapacity = dict()
         self.testsConstantCapacity = dict()
         self.__generateTests()
@@ -32,6 +33,7 @@ class TestCases:
                  for i in range(numberItems)], constant_weight]
 
 
+
 class Analyze:
     def __init__(self):
         self.functions = [Memory_Function, greedyAlgo, knapsack01_dp, annealing_algorithm, GeneticKnapsack().solve]
@@ -39,42 +41,89 @@ class Analyze:
         self.tests = TestCases()
         self.timeAnalyzer = MyTime()
         self.memoryAnalyzer = Memory()
+        self.averageCase = None
+        self.bestCase = None
+        self.worstCase = None
 
     def analyze(self, varyingCapacity = 1):
-        times = [[] for i in range(len(self.functions))]
-        memories = [[] for i in range(len(self.functions))]
-        size = []
-        if varyingCapacity:
-            cases = self.tests.testsVaryingCapacity
-            for n in cases:
-                print(n, "here")
-                size.append(n)
-                test_case  = cases[n]
-                for i in range(len(self.functions)):
-                    func = self.functions[i]
-                    self.timeAnalyzer.start()
-                    try:
-                        func(test_case[0], test_case[1])
-                    except RecursionError:
-                        print("Skipping")
-                    self.timeAnalyzer.stop()
-                    times[i].append(self.timeAnalyzer.elapsedTime)
-                    self.timeAnalyzer.reset()
-                    memories[i].append(Memory.memoryFootprint)
-                    Memory.resetMemory()
+        times = [[[] for i in range(len(self.functions))] for itr in range(10)]
+        memories = [[[] for i in range(len(self.functions))] for itr in range(10)]
+        size = list(TestCases().n)
+       	for itr in range(10):
+	        if varyingCapacity:
+	            cases = self.tests.testsVaryingCapacity
+	            for n in cases:
+	                print(n, "here")
+	                test_case  = cases[n]
+	                for i in range(len(self.functions)):
+	                    func = self.functions[i]
+	                    self.timeAnalyzer.start()
+	                    try:
+	                        func(test_case[0], test_case[1])
+	                    except RecursionError:
+	                        print("Skipping")
+	                    self.timeAnalyzer.stop()
+	                    times[itr][i].append(self.timeAnalyzer.elapsedTime)
+	                    self.timeAnalyzer.reset()
+	                    memories[itr][i].append(Memory.memoryFootprint)
+	                    Memory.resetMemory()
+	
+        times_ = dict()
+        memories_ = dict()
+       	for i, j in enumerate(self.functions):
+       		times_[self.names[i]] = [times[itr][i] for itr in range(10)]
+       		memories_[self.names[i]] = [memories[itr][i] for itr in range(10)]
+       	
+       	self.averageCaseTime = {i:[] for i in self.names}
+        self.bestCaseTime = {i:[] for i in self.names}
+        self.worstCaseTime = {i:[] for i in self.names}
+        self.averageCaseMem = {i:[] for i in self.names}
+        self.bestCaseMem = {i:[] for i in self.names}
+        self.worstCaseMem = {i:[] for i in self.names}
 
-        print(size, times, memories)
+       	for i, j in enumerate(self.functions):
+       		self.averageCaseTime[self.names[i]] = np.mean(np.array(times_[self.names[i]]), axis=0)
+       		self.bestCaseTime[self.names[i]] = np.min(np.array(times_[self.names[i]]), axis=0)
+       		self.worstCaseTime[self.names[i]] = np.max(np.array(times_[self.names[i]]), axis=0)
+       		self.averageCaseMem[self.names[i]] = np.mean(np.array(memories_[self.names[i]]), axis=0)
+       		self.bestCaseMem[self.names[i]] = np.min(np.array(memories_[self.names[i]]), axis=0)
+       		self.worstCaseMem[self.names[i]] = np.max(np.array(memories_[self.names[i]]), axis=0)
+
+
         for i in range(len(self.functions)):
-            plt.plot(size, times[i], label = self.names[i])
+            plt.plot(size, self.averageCaseTime[self.names[i]], label = self.names[i])
         plt.legend()
-        plt.title("Empirical Time")
+        plt.title("Empirical Time: Average Case")
         plt.show()
 
+        for i in range(len(self.functions)):
+            plt.plot(size, self.worstCaseTime[self.names[i]], label = self.names[i])
+        plt.legend()
+        plt.title("Empirical Time: Worst Case")
+        plt.show()
 
         for i in range(len(self.functions)):
-            plt.plot(size, memories[i], label = self.names[i])
+            plt.plot(size, self.bestCaseTime[self.names[i]], label = self.names[i])
         plt.legend()
-        plt.title("Memory Footprint")
+        plt.title("Empirical Time: Best Case")
+        plt.show()
+
+        for i in range(len(self.functions)):
+            plt.plot(size, self.averageCaseMem[self.names[i]], label = self.names[i])
+        plt.legend()
+        plt.title("Empirical Memory: Average Case")
+        plt.show()
+
+        for i in range(len(self.functions)):
+            plt.plot(size, self.worstCaseMem[self.names[i]], label = self.names[i])
+        plt.legend()
+        plt.title("Empirical Memory: Worst Case")
+        plt.show()
+
+        for i in range(len(self.functions)):
+            plt.plot(size, self.bestCaseMem[self.names[i]], label = self.names[i])
+        plt.legend()
+        plt.title("Empirical Memory: Best Case")
         plt.show()
 
         

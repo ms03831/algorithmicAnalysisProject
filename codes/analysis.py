1d5214b16b12f56e54eedf708da1175fcdd796d3
Mudasir <ms03831@st.habib.edu.pk>
2019-12-02 08:51:54 +0500

    some code updated

--- codes/analysis.py
+++ codes/analysis.py
@@ -17,7 +17,7 @@ sns.set_style("darkgrid", {"axes.facecolor": ".9", "lines.linewidth": 2.5})
 
 class TestCases:
     def __init__(self):
-        self.n = [10, 25, 50, 100, 500]#, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000]
+        self.n = [5, 10, 15, 20, 50, 100, 500]#, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000]
         self.testsVaryingCapacity = dict()
         self.testsConstantCapacity = dict()
         self.__generateTests()
@@ -36,8 +36,8 @@ class TestCases:
 
 class Analyze:
     def __init__(self):
-        self.functions = [Memory_Function, greedyAlgo, knapsack01_dp, annealing_algorithm, GeneticKnapsack().solve]
-        self.names = ["Memory Functions", "Greedy Algo", "DP", "Randomized Algorithm", "Genetic Knapsack"]
+        self.functions = [Memory_Function, greedyAlgo, knapsack01_dp, annealing_algorithm, GeneticKnapsack().solve, knapsack_brute_force]
+        self.names = ["Memory Functions", "Greedy Algo", "DP", "Randomized Algorithm", "Genetic Knapsack", "Brute Force"]
         self.tests = TestCases()
         self.timeAnalyzer = MyTime()
         self.memoryAnalyzer = Memory()
@@ -48,38 +48,48 @@ class Analyze:
     def analyze(self, varyingCapacity = 1):
         times = [[[] for i in range(len(self.functions))] for itr in range(10)]
         memories = [[[] for i in range(len(self.functions))] for itr in range(10)]
-        size = list(TestCases().n)
+        solutions = [[[] for i in range(len(self.functions))] for itr in range(10)]
+        size = list(TestCases().n)[:4]
        	for itr in range(10):
 	        if varyingCapacity:
 	            cases = self.tests.testsVaryingCapacity
 	            for n in cases:
-	                print(n, "here")
-	                test_case  = cases[n]
-	                for i in range(len(self.functions)):
-	                    func = self.functions[i]
-	                    self.timeAnalyzer.start()
-	                    try:
-	                        func(test_case[0], test_case[1])
-	                    except RecursionError:
-	                        print("Skipping")
-	                    self.timeAnalyzer.stop()
-	                    times[itr][i].append(self.timeAnalyzer.elapsedTime)
-	                    self.timeAnalyzer.reset()
-	                    memories[itr][i].append(Memory.memoryFootprint)
-	                    Memory.resetMemory()
+	            	if n < 50:
+		                print(n, "here")
+		                test_case  = cases[n]
+		                for i in range(len(self.functions)):
+		                    func = self.functions[i]
+		                    self.timeAnalyzer.start()
+		                    try:
+		                        s = func(test_case[0], test_case[1])
+		                        totVal = sum([test_case[0][j][2] for j in range(len(test_case[0])) if test_case[0][j][0] in s])
+		                        solutions[itr][i].append(totVal)
+
+		                    except RecursionError:
+		                        print("Skipping")
+		                    self.timeAnalyzer.stop()
+		                    times[itr][i].append(self.timeAnalyzer.elapsedTime)
+		                    self.timeAnalyzer.reset()
+		                    memories[itr][i].append(Memory.memoryFootprint)
+		                    Memory.resetMemory()
+	        print(solutions)
+	    #print(solutions)
 	
         times_ = dict()
         memories_ = dict()
+        solutions_ = dict()
        	for i, j in enumerate(self.functions):
        		times_[self.names[i]] = [times[itr][i] for itr in range(10)]
        		memories_[self.names[i]] = [memories[itr][i] for itr in range(10)]
-       	
+       		solutions_[self.names[i]] = [solutions[itr][i] for itr in range(10)]
+
        	self.averageCaseTime = {i:[] for i in self.names}
         self.bestCaseTime = {i:[] for i in self.names}
         self.worstCaseTime = {i:[] for i in self.names}
         self.averageCaseMem = {i:[] for i in self.names}
         self.bestCaseMem = {i:[] for i in self.names}
         self.worstCaseMem = {i:[] for i in self.names}
+        self.averageCaseSolution = {i:[] for i in self.names}
 
        	for i, j in enumerate(self.functions):
        		self.averageCaseTime[self.names[i]] = np.mean(np.array(times_[self.names[i]]), axis=0)
@@ -88,42 +98,66 @@ class Analyze:
        		self.averageCaseMem[self.names[i]] = np.mean(np.array(memories_[self.names[i]]), axis=0)
        		self.bestCaseMem[self.names[i]] = np.min(np.array(memories_[self.names[i]]), axis=0)
        		self.worstCaseMem[self.names[i]] = np.max(np.array(memories_[self.names[i]]), axis=0)
+       		self.averageCaseSolution[self.names[i]] = np.mean(np.array(solutions_[self.names[i]]), axis=0)
 
+       	plt.plot(size, self.averageCaseSolution["DP"], "r+", linewidth = 3, label = "Optimal Solution: DP")
+
+       	for i in range(len(self.functions)):
+       		if self.names[i] != "DP":
+       			plt.plot(size, self.averageCaseSolution[self.names[i]], label = self.names[i])
+       	plt.legend()
+        plt.title("Optimal Solution(DP) vs the rest")
+        plt.xlabel("number of items")
+        plt.ylabel("value")
+        
+        plt.show()
 
         for i in range(len(self.functions)):
             plt.plot(size, self.averageCaseTime[self.names[i]], label = self.names[i])
         plt.legend()
         plt.title("Empirical Time: Average Case")
+        plt.xlabel("number of items")
+        plt.ylabel("Time")
         plt.show()
 
         for i in range(len(self.functions)):
             plt.plot(size, self.worstCaseTime[self.names[i]], label = self.names[i])
         plt.legend()
         plt.title("Empirical Time: Worst Case")
+        plt.xlabel("number of items")
+        plt.ylabel("Time")
         plt.show()
 
         for i in range(len(self.functions)):
             plt.plot(size, self.bestCaseTime[self.names[i]], label = self.names[i])
         plt.legend()
         plt.title("Empirical Time: Best Case")
+        plt.xlabel("number of items")
+        plt.ylabel("Time")
         plt.show()
 
         for i in range(len(self.functions)):
             plt.plot(size, self.averageCaseMem[self.names[i]], label = self.names[i])
         plt.legend()
         plt.title("Empirical Memory: Average Case")
+        plt.xlabel("number of items")
+        plt.ylabel("Memory")
         plt.show()
 
         for i in range(len(self.functions)):
             plt.plot(size, self.worstCaseMem[self.names[i]], label = self.names[i])
         plt.legend()
         plt.title("Empirical Memory: Worst Case")
+        plt.xlabel("number of items")
+        plt.ylabel("Memory")
         plt.show()
 
         for i in range(len(self.functions)):
             plt.plot(size, self.bestCaseMem[self.names[i]], label = self.names[i])
         plt.legend()
         plt.title("Empirical Memory: Best Case")
+        plt.xlabel("number of items")
+        plt.ylabel("Memory")
         plt.show()
 
         
